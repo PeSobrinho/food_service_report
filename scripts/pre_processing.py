0f1a56d2ba18378b5ec9a70a6b1dad6f3c9e5d1b
PeSobrinho <pedrosobrinho.physics@gmail.com>
2024-09-07 13:41:22 -0300

    Iniciando dashboard

--- scripts/pre_processing.py
+++ scripts/pre_processing.py
@@ -28,7 +28,7 @@ def pre_processing_data(data):
     # Replacing 'OK' values in 'pedido_pago' column with 1, and other values with 0
 
     pre_processed_data['Pago'] = pre_processed_data['Pago'].replace('OK', 1)
-    pre_processed_data['Pago'] = pre_processed_data['Pago'].replace(pd.NA, 0)
+    pre_processed_data['Pago'] = pre_processed_data['Pago'].fillna(0)
 
 
     # change data types
